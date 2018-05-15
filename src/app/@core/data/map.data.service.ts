6217066512bf863fe6b432d3efbef9c8542290da
zmegh <elmeghni@gmail.com>
2018-05-15 14:44:38 -0400

    auto stream data

--- src/app/@core/data/map.data.service.ts
+++ src/app/@core/data/map.data.service.ts
@@ -3,24 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import * as appConfig from '../../../appSettings.json';
 
-
 @Injectable()
-export class TwitterUsersBackendService {
+export class MapDataService {
 
-  private rawJson: string;
-  private users: any[];
-  private observable: Observable<any>;
-  private url = appConfig['mapDataPath'];
+  private url = appConfig['userDataPath'];
 
   constructor(private http: HttpClient) {
     this.http = http;
   }
 
-  getContactByCountryName(countryName: string) {
-    return this.users.filter(u => u.country == countryName);
-  }
-
-  getData() {
+  getUserData() {
     return this.http.get(this.url);
   }
 
